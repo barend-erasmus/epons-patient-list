18e10e621ae7f81734d44caefcbb12746232033c
Barend Erasmus <developersworkspace@gmail.com>
2017-12-19 11:11:54 +0200

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,8 +14,18 @@ export class AppComponent {
 
   public user: any = {};
 
-  public patients: any = {};
+  public permission: any = {};
 
+  public facilityId: string;
+
+  public patients: any[] = [];
+
+  public pages: number[] = [];
+
+  public currentPage: number = 1;
+
+  public pageSize: number = 3;
+  
   constructor(private http: Http, private el: ElementRef) {
 
   }
@@ -23,6 +33,16 @@ export class AppComponent {
   public ngOnInit(): void {
     const userId = this.el.nativeElement.getAttribute('userId');
     this.loadUser(userId);
+  }
+
+  public onClick_PageNumber(page: number): void {
+    this.currentPage = page;
+
+    if (this.permission === 'Case Manager') {
+      this.loadActivePatients(null, this.facilityId);
+    } else {
+      this.loadActivePatients(this.user.Id, this.facilityId);
+    }
   }
 
   private loadUser(userId: string): void {
@@ -33,30 +53,36 @@ export class AppComponent {
     }).subscribe((json) => {
       this.user = json;
 
-      const facilityId = this.el.nativeElement.getAttribute('facilityId');
+      this.facilityId = this.el.nativeElement.getAttribute('facilityId');
 
-      const permission = this.user.Permissions.find((x) => x.Facility.Id === facilityId);
+      this.permission = this.user.Permissions.find((x) => x.Facility.Id === this.facilityId);
 
-      if (permission === 'Case Manager') {
-        this.loadActivePatients(null, facilityId);
-      }else {
-        this.loadActivePatients(userId, facilityId);
-      }      
+      if (this.permission === 'Case Manager') {
+        this.loadActivePatients(null, this.facilityId);
+      } else {
+        this.loadActivePatients(userId, this.facilityId);
+      }
     });
   }
 
   private loadActivePatients(userId: string, facilityId: string): void {
 
-    this.get(`/api/Patient/List?userId=${userId}&type=0&start=0&end=10&facilityId=${facilityId}`).map((x) => {
+    this.get(`/api/Patient/List?userId=${userId}&type=0&start=${(this.currentPage - 1) * this.pageSize}&end=${this.currentPage * this.pageSize}&facilityId=${facilityId}`).map((x) => {
       const json: any = x.json();
       return json;
     }).subscribe((json) => {
-      this.patients = json;
+      this.patients = json.Items;
 
-      this.patients.Items.forEach(element => {
+      this.patients.forEach(element => {
         element.DateOfBirth = new Date(element.DateOfBirth);
         element.FacilitiesName = element.Facilities.map((x) => x.Name).join(', ');
       });
+
+      this.pages = [];
+
+      for (let i = 1; i < Math.ceil(json.Count / this.pageSize) + 1; i++) {
+        this.pages.push(i);
+      }
     });
   }
 
